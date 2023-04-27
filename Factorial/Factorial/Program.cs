b301422237a4bf3350c8900a79b9d22380c5c332
Dávid Huljak <david.huljak@gmail.com>
2023-04-28 01:26:52 +0200

    Factorial

---
+++ Factorial/Factorial/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Factorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Napiš číslo");
+            int input = int.Parse(Console.ReadLine());
+            Console.WriteLine("Váš skvělý faktoriál je: " + getFactorial(input));
+            Console.ReadLine();
+
+        }
+
+        public static int getFactorial(int n)
+        {
+            if (n < 0)
+            {
+                Console.WriteLine("Negativni cislo");
+            }
+
+            int result = 1;
+
+            for (int i = n; i >= 1; i--)
+            {
+                result *= i;
+            }
+
+            return result;
+        }
+    }
+}
