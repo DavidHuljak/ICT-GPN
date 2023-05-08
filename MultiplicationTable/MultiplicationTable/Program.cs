8c4afa6b3e73ff6566e7dc61429be0ef9fe66c19
Dávid Huljak <david.huljak@gmail.com>
2023-05-08 18:47:06 +0200

    MultiplicationTable

---
+++ MultiplicationTable/MultiplicationTable/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MultiplicationTable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Multiplication Table";
+            Console.WriteLine("Enter number:");
+            int number = int.Parse(Console.ReadLine());
+            for (int i = 0; i <= number; i++)
+            {
+                for (int j = 0; j <= number; j++)
+                {
+                    Console.Write(i * j + " ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+
+        }
+
+    }
+
+}
